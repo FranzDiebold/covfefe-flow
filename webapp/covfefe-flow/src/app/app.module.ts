48910af6c63b0af679a8a2515298dd73a2f18e67
Franz Diebold <softwareentwicklung.diebold+github@gmail.com>
2018-03-26 12:00:50 +0200

    Fixed SVG gradients in Safari: Now setting 'base href' programmatically.

--- webapp/covfefe-flow/src/app/app.module.ts
+++ webapp/covfefe-flow/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APP_BASE_HREF } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,9 @@ import { AppRoutingModule } from './app-routing.module';
 
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
